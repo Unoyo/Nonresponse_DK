93b8b978fac3e00aff9054b6ee41433250fbbfd8
Unoyo <yosuke.uno0118@gmail.com>
2018-01-28 14:33:21 +0900

    mod

--- bagging_logistic.R
+++ bagging_logistic.R
@@ -12,7 +12,7 @@
     mydata <- as.data.frame(mydata)
     names(mydata)[i] <- paste("di",i,sep = "")
   }
-  mydata$inf_exp <- sample(0:1, size = N, replace = TRUE)
+  mydata$inf_exp <- sample(0:1, size = N, replace = TRUE, prob = c(3/4,1/4))
   mydata$ind <- as.factor(sample(1:100, size = N, replace = TRUE))
   mydata_p <- mydata %>%
     filter(inf_exp == 1)
@@ -22,21 +22,18 @@
   mydata_names <- names(mydata)
   f <- as.formula(paste("inf_exp~", paste(mydata_names[mydata_names != "inf_exp"], collapse = "+")))
   
-  length_divisor = 4
-  sampling <- sample(nrow(mydata_u), size = 2*floor((nrow(mydata_u))/length_divisor))
-  mydata_u_train <- mydata_u[sampling,]
-  mydata_u_test <- mydata_u[-sampling,]
-  mydata_train <- rbind(mydata_u_train, mydata_p)
-  mydata_test <- rbind(mydata_u_test, mydata_p)
-  
-  B = 100
+  m = 4
+  B = 10
   predictions <- foreach(m = 1:B, .combine = cbind) %do% {
-      train_pos <- sample(nrow(mydata_train), size = floor((nrow(mydata_train)/length_divisor)))
-      glm_fit <- glm(formula = f, data = mydata_train[train_pos,], family = binomial)
-      predict(glm_fit, newdata = mydata_test)
+      M <- sample(nrow(mydata_u), size = floor((nrow(mydata_u)/m)))
+      glm_fit <- glm(formula = f, 
+                     data = rbind(mydata_p, mydata_u[M,]),
+                     family = binomial)
+      predict(glm_fit, newdata = mydata_u, type = "response")
     }
   predictions <- rowMeans(predictions)
-  rmse <- sqrt((sum((mydata_test$inf_exp - predictions)^2))/nrow(mydata_test))
-  rmse
-  
+  plot(predictions)
+  u_p <- predictions[predictions > 0.5]
+  u_n <- predictions[predictions <= 0.5]
+
   proc.time() - Hajime
