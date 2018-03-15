f50b4eab4a88b4b823584ee2385a7fe11855998b
Unoyo <yosuke.uno0118@gmail.com>
2018-03-15 16:57:45 +0900

    mod

--- 2step_osvm.R
+++ 2step_osvm.R
@@ -5,7 +5,7 @@ suppressMessages(library(dplyr))
 suppressMessages(library(doParallel))
 
 mydata <- NULL
-N <- 10000
+N <- 100000
 
 foreach(i = 1:13, .inorder = FALSE) %do%{
   assign(paste("di",i,sep = ""),as.factor(sample(1:3, size = N, replace = TRUE)))
@@ -16,7 +16,7 @@ foreach(i = 1:13, .inorder = FALSE) %do%{
 mydata$inf_exp1 <- sample(0:1, size = N, replace = TRUE, prob = c(1/5,4/5))
 mydata$inf_exp3 <- sample(0:1, size = N, replace = TRUE, prob = c(2/5,3/5))
 mydata$inf_exp5 <- sample(0:1, size = N, replace = TRUE, prob = c(3/5,2/5))
-mydata$ind <- as.factor(sample(1:100, size = N, replace = TRUE))
+mydata$ind <- as.factor(sample(1:3, size = N, replace = TRUE))
 
 
 ## fraction of outlier ##
@@ -34,24 +34,41 @@ mydata_u <- mydata %>%
   filter(inf_exp1 == 0) %>%
   select(-outlier)
 
-nu <- 0.01#sum(mydata$outlier)/length(mydata$outlier)
+nu <- 0.01 #sum(mydata$outlier)/length(mydata$outlier)
 occ_svm <- ksvm(x = inf_exp1~., data = mydata_p, type = "one-svc", kernel = "rbfdot",
                 kpar = "automatic", C = 1/nu*N, nu = nu)
 occ_pred  <- as.numeric(predict(occ_svm, newdata = mydata_u))
-length(occ_pred)
+L <- length(occ_pred)
 
 ## step2 ##
-mydata_u_2 <- mydata_u %>%
-  mutate(occ_pred) %>%
-  filter(occ_pred == 0) %>%
-  select(-occ_pred)
-mydata_u_3 <- mydata_u %>%
-  mutate(occ_pred) %>%
-  filter(occ_pred == 1) %>%
-  select(-occ_pred)
+dif_U <- 1
+new_positive <- as.data.frame(occ_pred)
+colnames(new_positive) <- c("new_positive")
+mydata_u_temp_U <- as.data.frame(mydata_u)
 
-glm_fit <- glm(formula = inf_exp1~., data = rbind(mydata_p, mydata_u_2), family = binomial)
-pred <- predict(glm_fit, newdata = mydata_u_3, type = "response")
-new_RN <- pred[pred < 0.2]
-length(new_RN)
+repeat{
+  if(dif_U > 0){
+    U_size_temp <- length(mydata_u_temp_U$di1)
+    mydata_u_temp_N <- mydata_u_temp_U %>%
+      cbind(new_positive) %>%
+      filter(new_positive == 0) %>%
+      select(-new_positive)
+    mydata_u_temp_U <- mydata_u_temp_U %>%
+      cbind(new_positive) %>%
+      filter(new_positive == 1) %>%
+      select(-new_positive)
+    
+    sample(nrow(mydata_p), size = L)
+    glm_fit <- glm(formula = inf_exp1~., 
+                   data = rbind(mydata_p[L,], mydata_u_temp_N), 
+                   family = binomial)
+    pred <- predict(glm_fit, newdata = mydata_u_temp_U, type = "response")
+    new_positive <- pred >= 0.5
+    new_positive <- as.data.frame(new_positive)
+    colnames(new_positive) <- c("new_positive")
+    dif_U <- U_size_temp - length(mydata_u_temp_U$di1)
+  } else {
+    break
+  }
+}
 
