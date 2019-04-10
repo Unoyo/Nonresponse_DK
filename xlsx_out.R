d68c77fe427fba5c20db658ab8e5e7ab4fce2344
Unoyo <yosuke.uno0118@gmail.com>
2019-04-10 21:37:39 +0900

    Delete xlsx_out.R

--- xlsx_out.R
+++
@@ -1,7 +0,0 @@
-library(openxlsx)
-
-res_verification <- list("res_verification_gp1" = res_verification_gp1, 
-                         "res_verification_gp3" = res_verification_gp3,
-                         "res_verification_gp5" = res_verification_gp5)
-
-write.xlsx(res_verification, "res_verification.xlsx")
